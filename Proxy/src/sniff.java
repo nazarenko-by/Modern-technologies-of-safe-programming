d6efcd3711909398c129d672e71575901cc5a0e5
Bohdan <32818054+nazarenko-by@users.noreply.github.com>
2020-12-18 17:25:22 +0200

    Add files via upload

---
+++ Proxy/src/sniff.java
@@ -0,0 +1,48 @@
+private void forwardRequest(String method, HttpServletRequest req, HttpServletResponse resp) {
+        final boolean hasoutbody = (method.equals("POST"));
+
+        try {
+            final URL url = new URL(GlobalConstants.CLIENT_BACKEND_HTTPS 
+                    + req.getRequestURI()
+                    + (req.getQueryString() != null ? "?" + req.getQueryString() : ""));
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method);
+
+            final Enumeration<String> headers = req.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                final String header = headers.nextElement();
+                final Enumeration<String> values = req.getHeaders(header);
+                while (values.hasMoreElements()) {
+                    final String value = values.nextElement();
+                    conn.addRequestProperty(header, value);
+                }
+            }
+            conn.setUseCaches(false);
+            conn.setDoInput(true);
+            conn.setDoOutput(hasoutbody);
+            conn.connect();
+
+            final byte[] buffer = new byte[16384];
+            while (hasoutbody) {
+                final int read = req.getInputStream().read(buffer);
+                if (read <= 0) break;
+                conn.getOutputStream().write(buffer, 0, read);
+            }
+
+            resp.setStatus(conn.getResponseCode());
+            for (int i = 0; ; ++i) {
+                final String header = conn.getHeaderFieldKey(i);
+                if (header == null) break;
+                final String value = conn.getHeaderField(i);
+                resp.setHeader(header, value);
+            }
+
+            while (true) {
+                final int read = conn.getInputStream().read(buffer);
+                if (read <= 0) break;
+                resp.getOutputStream().write(buffer, 0, read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
